541f9e36ed267c64fadc366e5cf8bd2e4ae2832c
Robert Vojta <rvojta@me.com>
2019-09-25 12:21:58 +0200

    Crate separation
    Signed-off-by: Robert Vojta <rvojta@me.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,10 @@
-# Changes crossterm_style 0.4
+# Version 0.5.1
+
+- Maintenance release only
+- Moved to a [separate repository](https://github.com/crossterm-rs/crossterm-style)
+
+# Version 0.5.0
+
 - `get_available_color_count` returns no result
 - `ExecutableCommand::queue` returns `crossterm::Result`
 - `QueueableCommand::queue` returns `crossterm::Result`
@@ -6,14 +12,17 @@
 - Added derives: `Debug` for `ObjectStyle`  [debug-derive]
 - Command API takes mutable self instead of self
 
-# Changes crossterm_style 0.3
+# Version 0.3.0
+
 - Removed `TerminalColor::from_output()` 
 - Added `NoItalic` attribute
 
-# Changes crossterm_style 0.2
+# Version 0.2.0
+
 - Introduced more `Attributes`
 - Introduced easier ways to style text [issue 87](https://github.com/crossterm-rs/crossterm/issues/87).
 - Removed `ColorType` since it was unnecessary.
 
-# Changes crossterm_style 0.1 
+# Version 0.1.0
+
 - Moved out of `crossterm` 5.4 crate. 
--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,121 @@
-//! Have you ever been disappointed when a terminal library for rust was only written for UNIX systems?
-//! Crossterm provides clearing, input handling, styling, cursor movement, and terminal actions for both Windows and UNIX systems.
-//!
-//! Crossterm aims to be simple and easy to call in code.
-//! Through the simplicity of Crossterm, you do not have to worry about the platform you are working with.
-//!
-//! This crate supports all UNIX and Windows terminals down to Windows 7 (not all terminals are tested see [Tested Terminals](#tested-terminals) for more info).
-//!
-//! This crate consists of five modules that are provided behind [feature flags](https://crossterm-rs.github.io/crossterm/docs/feature_flags.html) so that you can define which features you'd like to have; by default, all features are enabled.
-//! - [Crossterm Style](https://crates.io/crates/crossterm_style)
-//! - [Crossterm Input](https://crates.io/crates/crossterm_input)
-//! - [Crossterm Screen](https://crates.io/crates/crossterm_screen)
-//! - [Crossterm Cursor](https://crates.io/crates/crossterm_cursor)
-//! - [Crossterm Terminal](https://crates.io/crates/crossterm_terminal)
+//! A module that contains all the actions related to the styling of the terminal.
+//! Like applying attributes to text and changing the foreground and background.
+#![deny(unused_imports)]
 
-#[cfg(feature = "cursor")]
-pub use crossterm_cursor::{
-    cursor, BlinkOff, BlinkOn, Down, Goto, Hide, Left, ResetPos, Right, SavePos, Show,
-    TerminalCursor, Up,
-};
-#[cfg(feature = "input")]
-pub use crossterm_input::{
-    input, AsyncReader, InputEvent, KeyEvent, MouseButton, MouseEvent, SyncReader, TerminalInput,
-};
-#[cfg(feature = "screen")]
-pub use crossterm_screen::{AlternateScreen, IntoRawMode, RawScreen};
-#[cfg(feature = "style")]
-pub use crossterm_style::{
-    color, style, Attribute, Color, Colored, Colorize, ObjectStyle, PrintStyledFont, SetAttr,
-    SetBg, SetFg, StyledObject, Styler, TerminalColor,
-};
-#[cfg(feature = "terminal")]
-pub use crossterm_terminal::{terminal, Clear, ClearType, ScrollDown, ScrollUp, SetSize, Terminal};
-pub use crossterm_utils::{
-    execute, queue, Command, ErrorKind, ExecutableCommand, Output, QueueableCommand, Result,
-};
+use std::fmt::Display;
 
-pub use self::crossterm::Crossterm;
+pub use crossterm_utils::{execute, queue, Command, ExecutableCommand, QueueableCommand, Result};
 
-mod crossterm;
+pub use self::color::{color, PrintStyledFont, SetAttr, SetBg, SetFg, TerminalColor};
+pub use self::enums::{Attribute, Color, Colored};
+pub use self::objectstyle::ObjectStyle;
+pub use self::styledobject::StyledObject;
+pub use self::traits::{Colorize, Styler};
+
+#[macro_use]
+mod macros;
+mod color;
+mod enums;
+pub mod objectstyle;
+pub mod styledobject;
+mod traits;
+
+mod ansi_color;
+#[cfg(windows)]
+mod winapi_color;
+
+/// This trait defines the actions that can be performed with terminal colors.
+/// This trait can be implemented so that a concrete implementation of the ITerminalColor can fulfill
+/// the wishes to work on a specific platform.
+///
+/// ## For example:
+///
+/// This trait is implemented for `WinApi` (Windows specific) and `ANSI` (Unix specific),
+/// so that color-related actions can be performed on both UNIX and Windows systems.
+trait ITerminalColor {
+    /// Set the foreground color to the given color.
+    fn set_fg(&self, fg_color: Color) -> Result<()>;
+    /// Set the background color to the given color.
+    fn set_bg(&self, fg_color: Color) -> Result<()>;
+    /// Reset the terminal color to default.
+    fn reset(&self) -> Result<()>;
+}
+
+/// This could be used to style a type that implements `Display` with colors and attributes.
+///
+/// # Example
+/// ```ignore
+/// // get a styled object which could be painted to the terminal.
+/// let styled_object = style("Some Blue colored text on black background")
+///     .with(Color::Blue)
+///     .on(Color::Black);
+///
+/// // print the styled text * times to the current screen.
+/// for i in 1..10
+/// {
+///     println!("{}", styled_object);
+/// }
+/// ```
+///
+/// # Important Remark
+///
+/// - Please checkout the documentation for `Colorizer` or `Styler`.
+/// Those types will make it a bit easier to style a string.
+pub fn style<'a, D: 'a>(val: D) -> StyledObject<D>
+where
+    D: Display + Clone,
+{
+    ObjectStyle::new().apply_to(val)
+}
+
+impl Colorize<&'static str> for &'static str {
+    // foreground colors
+    def_str_color!(fg_color: black => Color::Black);
+    def_str_color!(fg_color: dark_grey => Color::DarkGrey);
+    def_str_color!(fg_color: red => Color::Red);
+    def_str_color!(fg_color: dark_red => Color::DarkRed);
+    def_str_color!(fg_color: green => Color::Green);
+    def_str_color!(fg_color: dark_green => Color::DarkGreen);
+    def_str_color!(fg_color: yellow => Color::Yellow);
+    def_str_color!(fg_color: dark_yellow => Color::DarkYellow);
+    def_str_color!(fg_color: blue => Color::Blue);
+    def_str_color!(fg_color: dark_blue => Color::DarkBlue);
+    def_str_color!(fg_color: magenta => Color::Magenta);
+    def_str_color!(fg_color: dark_magenta => Color::DarkMagenta);
+    def_str_color!(fg_color: cyan => Color::Cyan);
+    def_str_color!(fg_color: dark_cyan => Color::DarkCyan);
+    def_str_color!(fg_color: white => Color::White);
+    def_str_color!(fg_color: grey => Color::Grey);
+
+    // background colors
+    def_str_color!(bg_color: on_black => Color::Black);
+    def_str_color!(bg_color: on_dark_grey => Color::DarkGrey);
+    def_str_color!(bg_color: on_red => Color::Red);
+    def_str_color!(bg_color: on_dark_red => Color::DarkRed);
+    def_str_color!(bg_color: on_green => Color::Green);
+    def_str_color!(bg_color: on_dark_green => Color::DarkGreen);
+    def_str_color!(bg_color: on_yellow => Color::Yellow);
+    def_str_color!(bg_color: on_dark_yellow => Color::DarkYellow);
+    def_str_color!(bg_color: on_blue => Color::Blue);
+    def_str_color!(bg_color: on_dark_blue => Color::DarkBlue);
+    def_str_color!(bg_color: on_magenta => Color::Magenta);
+    def_str_color!(bg_color: on_dark_magenta => Color::DarkMagenta);
+    def_str_color!(bg_color: on_cyan => Color::Cyan);
+    def_str_color!(bg_color: on_dark_cyan => Color::DarkCyan);
+    def_str_color!(bg_color: on_white => Color::White);
+    def_str_color!(bg_color: on_grey => Color::Grey);
+}
+
+impl Styler<&'static str> for &'static str {
+    def_str_attr!(reset => Attribute::Reset);
+    def_str_attr!(bold => Attribute::Bold);
+    def_str_attr!(underlined => Attribute::Underlined);
+    def_str_attr!(reverse => Attribute::Reverse);
+    def_str_attr!(dim => Attribute::Dim);
+    def_str_attr!(italic => Attribute::Italic);
+    def_str_attr!(negative => Attribute::Reverse);
+    def_str_attr!(slow_blink => Attribute::SlowBlink);
+    def_str_attr!(rapid_blink => Attribute::RapidBlink);
+    def_str_attr!(hidden => Attribute::Hidden);
+    def_str_attr!(crossed_out => Attribute::CrossedOut);
+}
